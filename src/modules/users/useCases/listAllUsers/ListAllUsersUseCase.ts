2548432176893d755e9ecf171ec6f022521b28c6
Igor Silva <osilvaigor@gmail.com>
2021-08-13 23:47:36 -0300

    Chapter 2 - Desafio 1: introdução ao SOLID.

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -2,14 +2,18 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+  user_id: string | string[];
 }
 
 class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const isUserAdmin = this.usersRepository.findById(user_id).admin === true;
+    if (!isUserAdmin) {
+      throw new Error("You need to be admin to list all users!");
+    }
+    return this.usersRepository.list();
   }
 }
 
